2c978afcf9fd7b4844814e14f037e25003c200ea
tanyaleepr <tanyaleepr@gmail.com>
2022-03-03 21:01:18 -0600

    debugged

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./apiRoutes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
